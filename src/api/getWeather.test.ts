cec014f9ed87322c1d8ae646327ede83862f33e3
burykot <stadopikseli@gmail.com>
2020-06-01 11:25:46 +0200

    testy

--- src/api/getWeather.test.ts
+++ src/api/getWeather.test.ts
@@ -4,7 +4,6 @@ import MockAdapter from 'axios-mock-adapter';
 import { getWeather } from './getWeather';
 import { getWeatherMock } from './getWeather.mock';
 
-
 describe('getWeather', () => {
     let mock: MockAdapter;
 
@@ -17,38 +16,38 @@ describe('getWeather', () => {
     });
 
     it('should return an object with correct values', () => {
-
-        mock.onGet('/weather')
-            .reply(200, getWeatherMock.correct);
-
+        mock.onGet(/weather/).reply(200, getWeatherMock.correct);
         const result = getWeather('correct');
-    
-        expect(result.success).toEqual(true);
-        expect(result.data.temperature).toEqual(306.22);
-        expect(result.data.weatherConditions).toEqual('Clouds');
-        expect(result.data.windSpeed).toEqual(1.57);
-        expect(result.data.icon).toEqual('04d');
-    })
+
+        return result.then(response => {
+            expect(response.success).toEqual(true);
+            expect(response.data.temperature).toEqual(306.22);
+            expect(response.data.weatherConditions).toEqual('Clouds');
+            expect(response.data.windSpeed).toEqual(1.57);
+            expect(response.data.icon).toEqual('04d');
+        })
+    });
 
     it('404 shoud result in /not found/ message', () => {
-
-        mock.onGet('/weather')
+        mock.onGet(/weather/)
             .reply(404, getWeatherMock.notFound);
 
         const result = getWeather('test');
+
+        return result.then(response => {
+            expect(response.success).toEqual(false);
+            expect(response.data.message).toEqual('Location not found');
+        })
+    });
+
+    it('should return an error on NerworkError', () => {
+        mock.onGet(/weather/).networkError();
+
+        const result = getWeather('irrelevant/url');
     
-        expect(result.success).toEqual(false);
-        expect(result.data.message).toEqual('City not found');
-    })
-
-    it('NetworkError should result in /error/ error', () => {
-
-        mock.onGet('/weather').networkError();
-
-        const result = getWeather('correct');
-    
-        expect(result.success).toEqual(false);
-        expect(result.data.message).toEqual('There was an error, try again');
-    })
-})
-
+        return result.then(response => {
+            expect(response.success).toEqual(false);
+            expect(response.data.message).toEqual('There was an error, try again');
+        })
+    });
+});
